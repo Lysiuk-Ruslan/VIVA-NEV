4ebeaf2ab988469fbf28b4b68b1c6bf67a8b18a7
Lisiuk-Ruslan <114481433+Lysiuk-Ruslan@users.noreply.github.com>
2025-02-13 18:13:49 +0200

    html

---
+++ src/index.js
@@ -0,0 +1,73 @@
+// Menu mobile
+(() => {
+    const mobileMenu = document.querySelector('.js-menu-container');
+    const openMenuBtn = document.querySelector('.js-open-menu');
+    const closeMenuBtn = document.querySelector('.js-close-menu');
+
+    const toggleMenu = () => {
+        const isMenuOpen =
+            openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+        openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+        mobileMenu.classList.toggle('is-open');
+
+        const scrollLockMethod = !isMenuOpen
+            ? 'disableBodyScroll'
+            : 'enableBodyScroll';
+        bodyScrollLock[scrollLockMethod](document.body);
+    };
+
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
+
+    // Close the mobile menu on wider screens if the device orientation changes
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+        if (!e.matches) return;
+        mobileMenu.classList.remove('is-open');
+        openMenuBtn.setAttribute('aria-expanded', false);
+        bodyScrollLock.enableBodyScroll(document.body);
+    });
+})();
+
+// Swiper myWorks
+
+let swiper = new Swiper('.swiper', {
+    loop: true,
+    initialSlide: 1,
+    centeredSlides: true,
+    centeredSlidesBounds: true,
+    // slidesPerView: 3,
+    slidesPerGroup: 1,
+    spaceBetween: 50,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 1,
+        },
+
+        768: {
+            slidesPerView: 2,
+        },
+
+        1200: {
+            slidesPerView: 3,
+        },
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+
+        clickable: true,
+
+        dynamicBullets: true,
+    },
+
+
+});
